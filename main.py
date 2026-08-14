098cfb597ecbb6fb478f0afd4678e646b30cb6ce
ThaisAlves02 <thaisgraziele56@gmail.com>
2026-08-14 20:58:20 -0300

    update

---
+++ main.py
@@ -0,0 +1,7 @@
+import customtkinter as ctk
+from tkinter import ttk, messengebox #ttk para fazer a tabela
+
+#===============================================
+#Configurações da janela principal
+#===============================================
+
